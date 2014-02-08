9079252a179285d1a029158aebca0bd93fc8ceb9
Kazuhito Hokamura <k.hokamura@gmail.com>
2014-02-08 17:04:34 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# iBeacon Reciever for Mac OS X
+
+Mac OSX 10.9以上、Node.js 0.10 以上
+
+依存モジュールのインストール
+
+```
+$ npm install
+```
+
+config.jsonにidを設定。必要であれば検索対象のUUIDを指定（指定しない場合は全てのUUIDが対象）
+
+```
+{
+  "id": "hokamura",
+  "uuid": ""
+}
+```
---
+++ app.js
@@ -0,0 +1,16 @@
+var bleacon = require('bleacon');
+var request = require('request');
+var config = require('./config');
+
+if (!config.id) {
+  console.error('Error: config.jsonにidを書いてね');
+  process.exit(1);
+}
+
+bleacon.startScanning(config.uuid || null);
+
+bleacon.on('discover', function(data) {
+  data.id = config.id;
+  console.log(data);
+  request.post('http://html5-export-hackathon-ibeacon.herokuapp.com/', { form: data });
+});
